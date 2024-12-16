b19c0aadb8e13308c2be871adeaa637342e5664e
tyst3273 <tyst3273@colorado.edu>
2024-12-16 15:56:06 -0700

    autocommit

--- stripes/simulated_anneal/run_electrons.py
+++ stripes/simulated_anneal/run_electrons.py
@@ -9,7 +9,6 @@ import os
 
 # template to get args from
 scf_template = 'scf_template.py'
-nscf_template = 'nscf_template.py'
 
 # initial unitcell
 pos = [[ 0.0, 0.0, 0.0]]
@@ -21,7 +20,7 @@ types = ['Cu']
 #num_sc = np.array([4,6,8,10,16,20],dtype=float)
 #num_holes = 2/num_sc
 
-num_sc = np.array([8])
+num_sc = np.array([4,6])
 num_holes = 2/num_sc
 
 step = 0
@@ -36,83 +35,19 @@ for ii, n in enumerate(num_sc):
         # start from afm ground state
         stripes = c_stripes(pos,vecs,types,h,[n,n,1])
         stripes.neel_order()
-        #stripes.write(f'{n}x{n}_model.txt')
+        stripes.write('model.txt')
         kwargs = stripes.get_model_kwargs()
 
         num_sites = len(kwargs['atom_types'])
         num_electrons = num_sites*(1-h)
-        tol = num_sites*5e-4
-        #start_temp = 1e-3+(num_sites-16)/48
-        #end_temp = start_temp/20
-
-        nk = round(40/n) 
-        if nk % 2 != 0:
-            nk += 1
-        kpts_mesh = [nk,nk,1]
-
-        print('num_electrons:',num_electrons)
-        print('tol:',tol)
-        print('kpts_mesh:',kpts_mesh)
-#        print('start_temp:',start_temp)
-#        print('end_temp:',end_temp)
 
         # scf
-        scf_output_file = f'scf_n_{n}_h_{h:.3f}_afm.hdf5'
+        scf_output_file = f'scf_n_{n}_h_{h:.3f}.hdf5'
         kwargs.update({'electron_output_file':scf_output_file,
-                       'num_electrons':num_electrons*(1-h),
-                       'kpts_mesh':kpts_mesh,
-                       'electron_scf_energy_tol':tol,
-                       'electron_scf_density_tol':tol})
-                       #'anneal_start_temperature':start_temp,
-                       #'anneal_end_temperature':end_temp})
-        ELPH = c_ELPH(scf_template)
-        ELPH.set_config(**kwargs)
-        ELPH.run()
-
-        exit()
-
-        # ------------------------------------------------------------------------------------------
-
-        # start from fm ground state
-        stripes.fm_order()
-        kwargs = stripes.get_model_kwargs()
-
-        # scf
-        scf_output_file = f'scf_n_{n}_h_{h:.3f}_fm.hdf5'
-        kwargs.update({'electron_output_file':scf_output_file,
-                       'num_electrons':num_electrons*(1-h),
-                       'kpts_mesh':kpts_mesh,
-                       'electron_scf_energy_tol':tol,
-                       'electron_scf_density_tol':tol})
-                       #'anneal_start_temperature':start_temp,
-                       #'anneal_end_temperature':end_temp})
+                       'num_electrons':num_electrons})
         ELPH = c_ELPH(scf_template)
         ELPH.set_config(**kwargs)
         ELPH.run()
 
         # ------------------------------------------------------------------------------------------
-
-        # start from pm ground state
-        stripes.pm_order()
-        kwargs = stripes.get_model_kwargs()
-
-        # scf
-        scf_output_file = f'scf_n_{n}_h_{h:.3f}_pm.hdf5'
-        kwargs.update({'electron_output_file':scf_output_file,
-                       'num_electrons':num_electrons*(1-h),
-                       'kpts_mesh':kpts_mesh,
-                       'electron_scf_energy_tol':tol,
-                       'electron_scf_density_tol':tol})
-                       #'anneal_start_temperature':start_temp,
-                       #'anneal_end_temperature':end_temp})
-        ELPH = c_ELPH(scf_template)
-        ELPH.set_config(**kwargs)
-        ELPH.run()
-
-        # ------------------------------------------------------------------------------------------
-
-        step += 1
-
-
-
 
--- stripes/simulated_anneal/scf_template.py
+++ stripes/simulated_anneal/scf_template.py
@@ -1,5 +1,5 @@
 
-debug = True #False
+#debug = True 
 
 task = 'electrons'
 temperature = 0.01
@@ -21,18 +21,16 @@ electron_scf_density_tol = 5e-3
 electron_scf_energy_tol = 1e-3
 
 electron_mix_method = 'simple'
-electron_mix_alpha = 0.4
-electron_mix_beta = 0.2
-electron_mix_delay = 10
-electron_mix_history = 4
+electron_mix_alpha = 0.6
 
 write_electron_eigenvectors = False
 write_site_density = True
 
 do_simulated_annealing = True
 num_anneal_steps = 50
-anneal_start_temperature = 0.01
-anneal_end_temperature = 0.001
+anneal_start_temperature = 0.1
+anneal_end_temperature = 0.01
 anneal_step_size = 0.5
 
 atom_files = ['Cu.py']
+
